c8646e330d3bb262a4af90e7630212c370eaeb84
aonypchenko <94485532+aonypchenko@users.noreply.github.com>
2022-11-25 18:28:32 +0200

    create endpoint POST

--- src/main/java/com/example/carservice/controller/CarController.java
+++ src/main/java/com/example/carservice/controller/CarController.java
@@ -1,11 +1,15 @@
 package com.example.carservice.controller;
 
 import com.example.carservice.model.Car;
+import com.example.carservice.model.EngineType;
+import com.example.carservice.repository.CarRepository;
 import com.example.carservice.service.CarService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -13,13 +17,30 @@ import java.util.List;
 public class CarController {
 
     private final CarService service;
+    private final CarRepository carRepository;
 
-    public CarController(CarService service) {
+    public CarController(CarService service, CarRepository carRepository) {
         this.service = service;
+        this.carRepository = carRepository;
     }
 
     @GetMapping
     public List<Car> getCars() {
         return service.getCars();
     }
+
+    @PostMapping("add")
+    public String addCars(){
+        Car Lexus = new Car();
+        Lexus.setModel("IS-250");
+        Lexus.setColor("white");
+        Lexus.setPrice(15000);
+        Lexus.setEngine(EngineType.GASOLINE);
+        Lexus.setPower(200);
+        Lexus.setEngineVolume(2.5);
+        Lexus.setIssueDate(LocalDate.ofEpochDay(2022-11-24));
+        carRepository.save(Lexus);
+        return Lexus.getModel();
+    }
+
 }
