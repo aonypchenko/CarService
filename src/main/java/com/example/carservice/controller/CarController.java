da7e8b6c50028c6b8dcf371b27e275f9ca553fd1
aonypchenko <94485532+aonypchenko@users.noreply.github.com>
2022-11-26 18:46:15 +0200

    new implementation of POST endpoint

--- src/main/java/com/example/carservice/controller/CarController.java
+++ src/main/java/com/example/carservice/controller/CarController.java
@@ -1,7 +1,6 @@
 package com.example.carservice.controller;
 
 import com.example.carservice.model.Car;
-import com.example.carservice.model.EngineType;
 import com.example.carservice.repository.CarRepository;
 import com.example.carservice.service.CarService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -30,17 +28,8 @@ public class CarController {
     }
 
     @PostMapping("add")
-    public String addCars(){
-        Car Lexus = new Car();
-        Lexus.setModel("IS-250");
-        Lexus.setColor("white");
-        Lexus.setPrice(15000);
-        Lexus.setEngine(EngineType.GASOLINE);
-        Lexus.setPower(200);
-        Lexus.setEngineVolume(2.5);
-        Lexus.setIssueDate(LocalDate.ofEpochDay(2022-11-24));
-        carRepository.save(Lexus);
-        return Lexus.getModel();
+    public Car addCars(){
+        return service.addCar();
     }
 
 }
