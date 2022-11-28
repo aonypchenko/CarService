b52e5f141c9382b5a390a4d98085025b254ba025
aonypchenko <94485532+aonypchenko@users.noreply.github.com>
2022-11-28 22:18:07 +0200

    create Delete endpoint

--- src/main/java/com/example/carservice/controller/CarController.java
+++ src/main/java/com/example/carservice/controller/CarController.java
@@ -3,10 +3,7 @@ package com.example.carservice.controller;
 import com.example.carservice.model.Car;
 import com.example.carservice.repository.CarRepository;
 import com.example.carservice.service.CarService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +22,10 @@ public class CarController {
     @GetMapping
     public List<Car> getCars() {
         return service.getCars();
+    }
+    @DeleteMapping("delete")
+    public void deleteCar(@RequestParam(value="id") Long id){
+        service.removeById(id);
     }
 
 }
